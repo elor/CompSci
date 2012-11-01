6204dbdedcabb9c1b5f07d5647404773da2418a7
Erik Lorenz <elor@hrz.tu-chemnitz.de>
2012-11-01 11:32:16 +0100

    end of lecture

--- code/2_OSPTest/src/exercise2/PendulumSimulationApp.java
+++ code/2_OSPTest/src/exercise2/PendulumSimulationApp.java
@@ -40,14 +40,16 @@ public class PendulumSimulationApp extends AbstractSimulation {
 
   @Override
   public void reset() {
-    control.setValue("Maximalwinkel (rad)", 0.1 * Math.PI);
-    control.setValue("Maximalgeschwindigkeit (rad/s)", 2.0);
-    control.setValue("Pendellaenge (m)", 0.1);
+    control.setValue("Minimalwinkel (rad)", 0.0);
+    control.setValue("Minimalgeschwindigkeit (rad/s)", 0.0);
+    control.setValue("Maximalwinkel (rad)", 1.0);
+    control.setValue("Maximalgeschwindigkeit (rad/s)", 1.0);
+    control.setValue("Pendellaenge (m)", 1.0);
     control.setValue("Anzahl", 1000);
     control.setValue("Zufaellig", false);
-    control.setAdjustableValue("Zeitschrittweite", 0.01);
+    control.setAdjustableValue("Zeitschrittweite", 0.001);
     enableStepsPerDisplay(true);
-    setStepsPerDisplay(10);
+    setStepsPerDisplay(100);
   }
 
   // control.println("Simulation stopped");
@@ -68,6 +70,8 @@ public class PendulumSimulationApp extends AbstractSimulation {
   public void initialize() {
     double thetamax = control.getDouble("Maximalwinkel (rad)");
     double omegamax = control.getDouble("Maximalgeschwindigkeit (rad/s)");
+    double thetamin = control.getDouble("Minimalwinkel (rad)");
+    double omegamin = control.getDouble("Minimalgeschwindigkeit (rad/s)");
     double length = control.getDouble("Pendellaenge (m)");
     int num = control.getInt("Anzahl");
     Boolean random = control.getBoolean("Zufaellig");
@@ -82,41 +86,43 @@ public class PendulumSimulationApp extends AbstractSimulation {
       plot = new PlotFrame("theta", "omega", "phase space");
     }
     plot.clearDrawables();
-    plot.setPreferredMinMax(-0.15 * Math.PI, 0.15 * Math.PI, -3, 3);
+    plot.setPreferredMinMax(-(thetamax + thetamin) * 1.5,
+        (thetamax + thetamin) * 1.5, -(omegamin + omegamax) * 1.5,
+        (omegamin + omegamax) * 1.5);
 
     pendulae = new PendulumODE[num];
-    solvers = new Verlet[num];
+    solvers = new AbstractODESolver[num];
     Random rand = new Random();
     for (int i = 0; i < num; ++i) {
       double theta = 0.0;
       double omega = 0.0;
       if (random) {
-        theta = (rand.nextDouble() * 2.0 - 1.0) * thetamax;
-        omega = (rand.nextDouble() * 2.0 - 1.0) * omegamax;
+        theta = rand.nextDouble() * thetamax;
+        omega = rand.nextDouble() * omegamax;
       } else {
         int quadrant = (4 * i) / num;
         double pos = new Double((4 * i) - quadrant * num) / num;
         switch (quadrant) {
         case 0:
-          theta = thetamax * 0.5;
-          omega = (pos * omegamax + omegamax) * 0.5;
+          theta = thetamin;
+          omega = omegamin + pos * omegamax;
           break;
         case 1:
-          theta = (pos * thetamax + thetamax) * 0.5;
-          omega = omegamax;
+          theta = thetamin + pos * thetamax;
+          omega = omegamin + omegamax;
           break;
         case 2:
-          theta = thetamax;
-          omega = ((1 - pos) * omegamax + omegamax) * 0.5;
+          theta = thetamin + thetamax;
+          omega = omegamin + (1 - pos) * omegamax;
           break;
         case 3:
-          theta = ((1 - pos) * thetamax + thetamax) * 0.5;
-          omega = omegamax * 0.5;
+          theta = thetamin + (1 - pos) * thetamax;
+          omega = omegamin;
         }
       }
 
       pendulae[i] = new PendulumODE(theta, omega, length, 9.81);
-      solvers[i] = new Verlet(pendulae[i]);
+      solvers[i] = new RK4(pendulae[i]);
       plot.addDrawable(pendulae[i]);
     }
     control.println("Simulation started");
