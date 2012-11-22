b1c102e6902c222eb5be88227909bbef355ebc25
Erik Lorenz <elor@hrz.tu-chemnitz.de>
2012-11-22 11:31:30 +0100

    end of exercise

--- code/2_OSP/exercise5/Billiard.java
+++ code/2_OSP/exercise5/Billiard.java
@@ -1,20 +1,24 @@
 package exercise5;
 
+import java.awt.Color;
 import java.awt.Graphics;
+import java.util.Random;
 
 import org.opensourcephysics.display.Drawable;
 import org.opensourcephysics.display.DrawingPanel;
 
 public class Billiard implements Drawable {
   double r, l;
-  double x, y, px, py;
+  double state[];
   double t;
   private double l2;
+  Random rand;
 
   /**
    * empty default constructor. Values are set using specific setters
    */
   public Billiard() {
+    rand = new Random();
   }
 
   /**
@@ -25,41 +29,70 @@ public class Billiard implements Drawable {
    *          radius of the caps
    * @param l
    *          length of the middle part
-   * @param x
-   *          x position
-   * @param y
-   *          y position
-   * @param px
-   *          x momentum
-   * @param py
-   *          y momentum
+   * @param balls
+   *          number of balls
    */
-  public void setProperties(double r, double l, double x, double y, double px,
-      double py) {
+  public void setProperties(double r, double l, int balls) {
     this.r = r;
     this.l = l;
     this.l2 = l / 2;
-    this.x = x;
-    this.y = y;
-    this.px = px;
-    this.py = py;
 
-    normalize();
+    state = new double[balls * 4 + 1];
   }
 
   /**
-   * Normalizes the momentum
+   * randomize all ball positions and
    */
-  public void normalize() {
-    double length = Math.sqrt(px * px + py * py);
+  public void randomize() {
+    int numballs = (state.length - 1) / 4;
+    double x, y;
 
-    if (length == 0.0) {
-      px = 1.0;
-      py = 0.0;
+    for (int ball = 0; ball < numballs; ++ball) {
+      do {
+        x = rand.nextDouble() * (l + 2 * r) - (l2 + r);
+        y = rand.nextDouble() * 2 * r - r;
+        setBall(ball, x, y, rand.nextDouble() - 0.5, rand.nextDouble() - 0.5);
+      } while (isInside(ball) == false);
     }
 
-    px /= length;
-    py /= length;
+    // reset the time
+    state[state.length - 1] = 0.0;
+  }
+
+  /**
+   * set the properties of a ball
+   * 
+   * @param ball
+   *          index of the ball (from 0)
+   * @param x
+   * @param y
+   * @param vx
+   * @param vy
+   */
+  public void setBall(int ball, double x, double y, double vx, double vy) {
+    // get offset within state vector
+    ball *= 4;
+
+    // avoid segfaults
+    if (ball > state.length - 5) {
+      return;
+    }
+
+    // normalize
+    double scale = Math.sqrt(vx * vx + vy * vy);
+    if (scale <= 1e-9) {
+      vx = 1.0;
+      vy = 0.0;
+    } else {
+      vx /= scale;
+      vy /= scale;
+    }
+
+    // apply
+    state[ball] = x;
+    state[ball + 1] = vx;
+    state[ball + 2] = y;
+    state[ball + 3] = vy;
   }
 
   /**
@@ -67,14 +100,19 @@ public class Billiard implements Drawable {
    * 
    * @return sector (1, 2, 3 if inside, 0 if outside)
    */
-  public int getSector() {
+  public int getSector(int ball) {
+    ball *= 4;
+    double x = state[ball];
+    double y = state[ball + 2];
+
+    // first sector (left)
     if (x < -l2) {
       double dist2 = Math.pow(x + l2, 2) + Math.pow(y, 2);
       if (dist2 > r * r) {
         return 0;
       }
       return 1;
-    } else if (x > l2) {
+    } else if (x <= l2) {
       if (y > r || y < -r) {
         return 0;
       }
@@ -93,8 +131,8 @@ public class Billiard implements Drawable {
    * 
    * @return true if the position (x, y) is inside, false otherwise
    */
-  public Boolean isInside() {
-    return getSector() != 0;
+  public Boolean isInside(int ball) {
+    return getSector(ball) != 0;
   }
 
   /**
@@ -105,9 +143,9 @@ public class Billiard implements Drawable {
     // TODO Auto-generated method stub
 
     int y1 = panel.yToPix(r);
-    int x1 = panel.xToPix(-l / 2 - r);
-    int rx = Math.abs(panel.xToPix(2*r) - panel.xToPix(0));
-    int ry = Math.abs(panel.yToPix(2*r) - panel.yToPix(0));
+    int x1 = panel.xToPix(-l2 - r);
+    int rx = Math.abs(panel.xToPix(2 * r) - panel.xToPix(0));
+    int ry = Math.abs(panel.yToPix(2 * r) - panel.yToPix(0));
 
     int x2, y2;
 
@@ -115,8 +153,8 @@ public class Billiard implements Drawable {
     g.drawArc(x1, y1, rx, ry, 90, 180);
 
     // second sector
-    x1 = panel.xToPix(-l / 2);
-    x2 = panel.xToPix(l / 2);
+    x1 = panel.xToPix(-l2);
+    x2 = panel.xToPix(l2);
     y1 = panel.yToPix(r);
     y2 = panel.yToPix(-r);
 
@@ -124,17 +162,264 @@ public class Billiard implements Drawable {
     g.drawLine(x1, y2, x2, y2);
 
     // third sector
-    x1 = panel.xToPix(l / 2 - r);
+    x1 = panel.xToPix(l2 - r);
     y1 = panel.yToPix(r);
-    rx = Math.abs(panel.xToPix(2*r) - panel.xToPix(0));
-    ry = Math.abs(panel.yToPix(2*r) - panel.yToPix(0));
+    rx = Math.abs(panel.xToPix(2 * r) - panel.xToPix(0));
+    ry = Math.abs(panel.yToPix(2 * r) - panel.yToPix(0));
 
     g.drawArc(x1, y1, rx, ry, 270, 180);
+
+    g.setColor(Color.blue);
+
+    // draw balls
+    for (int ball4 = 0; ball4 < state.length - 1; ball4 += 4) {
+      x1 = panel.xToPix(state[ball4]);
+      y1 = panel.yToPix(state[ball4 + 2]);
+      x2 = panel.xToPix(state[ball4] + state[ball4 + 1]);
+      y2 = panel.yToPix(state[ball4 + 2] + state[ball4 + 3]);
+
+      g.fillOval(x1 - 5, y1 - 5, 10, 10);
+      g.drawLine(x1, y1, x2, y2);
+    }
+  }
+
+  public double veclength(double x, double y) {
+    return Math.sqrt(x * x + y * y);
+  }
+
+  double solveQuadratic(double p, double q) {
+    if (p * p / 4 - q < 0.0) {
+      throw new RuntimeException(
+          "Impossibru! solveQuadratic: Starting outside the circle");
+    }
+
+    // -p/2 - Math.sqrt() is invalid, because it's in the past (since you
+    // entered the circle at some time)
+
+    return -p / 2 + Math.sqrt(p * p / 4 - q);
+  }
+
+  double getBounceTime(int ball) {
+    int ball4 = ball * 4;
+    while (getSector(ball) == 0) {
+      state[ball4] -= 1e-5 * state[ball4 + 1];
+      state[ball4 + 2] -= 1e-5 * state[ball4 + 3];
+    }
+    int sector = getSector(ball);
+    double time1 = 0.0, time2 = 0.0, timesum = 0.0;
+
+    double x = state[ball4];
+    double vx = state[ball4 + 1];
+    double y = state[ball4 + 2];
+    double vy = state[ball4 + 3];
+    double x0;
+
+    if (Math.abs(vx) < 1e5) {
+      switch (sector) {
+      case 1:
+        x0 = x + l2; // x relative to the center of the circle
+        return solveQuadratic(y * vy, x0 * x0 + y * y - r * r);
+      case 2:
+        if (vy > 0.0) {
+          // top
+          return (r - y) / vy;
+        } else {
+          return (-r - y) / vy;
+          // bottom
+        }
+      case 3:
+        x0 = x - l2; // x relative to the center of the circle
+        return solveQuadratic(y * vy, x0 * x0 + y * y - r * r);
+      }
+    } else if (vx > 0) {
+      // ball moves right
+      switch (sector) {
+      case 1:
+        // time when the ball enters the second sector
+        time1 = (-l2 - x) / vx; // always > 0.0;
+        // time when the ball hits the circle. solve the quadratic equation to
+        // obtain it
+        x0 = x + l2; // x relative to the center of the circle
+        time2 = solveQuadratic(y * vy + x0 * vx, x0 * x0 + y * y - r * r);
+        if (time1 >= time2 && time2 > 0.0) {
+          // bounced off the wall
+          if (vx * x0 + vy * y > 0.0) {
+            return time2;
+          }
+        }
+
+        // advance to a valid starting point for the second section
+        x += vx * time1;
+        y += vy * time1;
+        // store the time we already advanced
+        timesum += time1;
+        // fallthrough to the second section
+      case 2:
+        // time when the ball enters the third sector
+        time1 = (l2 - x) / vx;
+
+        // time when the ball hits the top or bottom line
+        if (vy > 0.0) {
+          // top
+          time2 = (r - y) / vy;
+        } else {
+          time2 = (-r - y) / vy;
+          // bottom
+        }
+
+        if (time1 >= time2) {
+          return timesum + time2;
+        }
+
+        // advance to a valid starting point for the third section
+        x += vx * time1;
+        y += vy * time1;
+        // store the time we already advanced
+        timesum += time1;
+        // fallthrough to the third section
+      case 3:
+        x0 = x - l2; // adjust x
+        // time when the border of the circle is hit
+        time1 = solveQuadratic(y * vy + x0 * vx, x0 * x0 + y * y - r * r);
+        return timesum + time1;
+      }
+    } else {
+      // ball moves left
+      switch (sector) {
+      case 3:
+        // time when the ball enters the second sector
+        time1 = (l2 - x) / vx; // always > 0.0;
+        // time when the ball hits the circle. solve the quadratic equation to
+        // obtain it
+        x0 = x - l2; // x relative to the center of the circle
+        time2 = solveQuadratic(y * vy + x0 * vx, x0 * x0 + y * y - r * r);
+        if (time1 >= time2 && time2 > 0.0) {
+          // bounced off the wall
+          if (vx * x0 + vy * y > 0.0) {
+            return time2;
+          }
+        }
+
+        // advance to a valid starting point for the second section
+        x += vx * time1;
+        y += vy * time1;
+        // store the time we already advanced
+        timesum += time1;
+        // fallthrough to the second section
+      case 2:
+        // time when the ball enters the third sector
+        time1 = (-l2 - x) / vx;
+
+        // time when the ball hits the top or bottom line
+        if (vy > 0.0) {
+          // top
+          time2 = (r - y) / vy;
+        } else {
+          time2 = (-r - y) / vy;
+          // bottom
+        }
+
+        if (time1 >= time2) {
+          return timesum + time2;
+        }
+
+        // advance to a valid starting point for the third section
+        x += vx * time1;
+        y += vy * time1;
+        // store the time we already advanced
+        timesum += time1;
+        // fallthrough to the third section
+      case 1:
+        x0 = x + l2; // adjust x
+        // time when the border of the circle is hit
+        time1 = solveQuadratic(y * vy + x0 * vx, x0 * x0 + y * y - r * r);
+        return timesum + time1;
+      }
+    }
+    return 0.0;
   }
 
   public void doStep() {
-    // TODO Auto-generated method stub
+    int minball = 0;
+    double minstep = 0.02;
+    double mintime = (2 * r + l) / 1.0; // t=s/v with normalized v: initialize
+                                        // with worst case time
+    double time;
 
+    int numballs = (state.length - 1) / 4;
+    for (int ball = 0; ball < numballs; ++ball) {
+      time = getBounceTime(0);
+      if (time < mintime) {
+        mintime = time;
+        minball = ball;
+      }
+    }
+
+    if (mintime < minstep) {
+      advanceState(mintime);
+      System.out.println(mintime);
+      bounceBall(minball);
+    } else {
+      advanceState(minstep);
+    }
+  }
+
+  private void advanceState(double timestep) {
+    int numballs = (state.length - 1) / 4;
+    int ball4;
+    for (int ball = 0; ball < numballs; ++ball) {
+      ball4 = ball * 4;
+      state[ball4] += state[ball4 + 1] * timestep;
+      state[ball4 + 2] += state[ball4 + 3] * timestep;
+    }
+    state[state.length - 1] += timestep;
+  }
+
+  private void bounceBall(int ball) {
+    int ball4 = ball * 4;
+
+    while (getSector(ball) == 0) {
+      state[ball4] -= 1e-5 * state[ball4 + 1];
+      state[ball4 + 2] -= 1e-5 * state[ball4 + 3];
+    }
+
+    double rx = state[ball4];
+    double vx = state[ball4 + 1];
+    double ry = state[ball4 + 2];
+    double vy = state[ball4 + 3];
+    double vr;
+
+    switch (getSector(ball)) {
+    case 1:
+      // first sector: bounce off circle
+      rx += l2;
+      vr = -2 * (rx * vx + ry * vy) / (rx * rx + ry * ry);
+      vx += rx * vr;
+      vy += ry * vr;
+
+      state[ball4 + 1] = vx;
+      state[ball4 + 3] = vy;
+      break;
+    case 2:
+      // second sector: invert the y movement
+      state[ball4 + 3] = -vy;
+      break;
+    case 3:
+      // third sector: bounce off circle
+      rx -= l2;
+      vr = -2 * (rx * vx + ry * vy) / (rx * rx + ry * ry);
+      vx += rx * vr;
+      vy += ry * vr;
+
+      state[ball4 + 1] = vx;
+      state[ball4 + 3] = vy;
+      break;
+    case 0:
+      // outside: kill the program
+      throw new RuntimeException("ball left space");
+    }
+
+    return;
   }
 
   public double getTime() {
--- code/2_OSP/exercise5/BilliardApp.java
+++ code/2_OSP/exercise5/BilliardApp.java
@@ -10,28 +10,13 @@ package exercise5;
 import org.opensourcephysics.controls.*;
 import org.opensourcephysics.frames.*;
 
-/**
- * Planet2App models and displays two interacting planets in the presence of a
- * central inverse square law force.
- * 
- * This application demonstrates:
- * <ol>
- * <li>how to use the ODE interface.</li>
- * <li>how to use the ODESolver interface.</li>
- * <li>how to use the Animation control to run and single-step a differential
- * equation.</li>
- * </ol>
- * Students should test other ODESolvers in the Numerics package.
- * 
- * @author Wolfgang Christian, Jan Tobochnik, Harvey Gould
- * @version 1.0
- */
 public class BilliardApp extends AbstractSimulation {
-  PlotFrame frame = new PlotFrame("x (AU)", "y (AU)", "Planet Program");
+  PlotFrame frame = new PlotFrame("x (AU)", "y (AU)", "Billiard Table");
   Billiard billiard = new Billiard();
-  
+  double state[];
+
   /**
-   * Constructs the PlanetApp.
+   * Constructor 
    */
   public BilliardApp() {
     frame.addDrawable(billiard);
@@ -49,7 +34,7 @@ public class BilliardApp extends AbstractSimulation {
       billiard.doStep(); // advances time
     }
 
-//    double time = billiard.getTime();
+    // double time = billiard.getTime();
   }
 
   /**
@@ -58,12 +43,10 @@ public class BilliardApp extends AbstractSimulation {
   public void initialize() {
     double r = control.getDouble("r");
     double l = control.getDouble("l");
-    double x = control.getDouble("x");
-    double y = control.getDouble("y");
-    double px = control.getDouble("px");
-    double py = control.getDouble("py");
-    billiard.setProperties(r, l, x, y, px, py);
-//    frame.setMessage("t=0");
+    int balls = control.getInt("balls");
+    billiard.setProperties(r, l, balls);
+    billiard.randomize();
+    // frame.setMessage("t=0");
   }
 
   /**
@@ -72,10 +55,7 @@ public class BilliardApp extends AbstractSimulation {
   public void reset() {
     control.setValue("r", 1);
     control.setValue("l", 2);
-    control.setValue("x", 0.1);
-    control.setValue("y", 1);
-    control.setValue("px", 1);
-    control.setValue("py", 0);
+    control.setValue("balls", 1);
     control.setAdjustableValue("calculations per step", 5);
     initialize();
   }
@@ -90,27 +70,3 @@ public class BilliardApp extends AbstractSimulation {
     SimulationControl.createApp(new BilliardApp());
   }
 }
-
-/*
- * Open Source Physics software is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License (GPL) as
- * published by the Free Software Foundation; either version 2 of the License,
- * or(at your option) any later version.
- * 
- * Code that uses any portion of the code in the org.opensourcephysics package
- * or any subpackage (subdirectory) of this package must must also be be
- * released under the GNU GPL license.
- * 
- * This software is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
- * 
- * You should have received a copy of the GNU General Public License along with
- * this; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
- * Suite 330, Boston MA 02111-1307 USA or view the license online at
- * http://www.gnu.org/copyleft/gpl.html
- * 
- * Copyright (c) 2007 The Open Source Physics project
- * http://www.opensourcephysics.org
- */
